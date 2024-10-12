5fc3c75e991ba01e10cca0f3f92350b2ac7f48f0
Sufianmahmood <100502783+Sufianmahmood@users.noreply.github.com>
2024-10-12 14:11:49 +0000

    datatypes

---
+++ 01_basics/varaible.js
@@ -0,0 +1,16 @@
+const accountId = 9971
+let accountEmail ="abcd@gmail.com"
+var accountPassaword="sufian123"
+accountcity = "Delhi"
+
+//accountId = 2
+
+accountEmail ="def@gmail.com"
+accountPassaword = "2458"
+accountcity = "chennai"
+
+/* 
+not use var because of problem in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassaword, accountcity]);
---
+++ datatype.js
@@ -0,0 +1,4 @@
+"use strict";//treat all code as newer version
+
+//alert(3+3) // we are using node not browser
+
