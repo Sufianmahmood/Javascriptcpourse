892340a2542e3d32e2deb5aaffc3301b0120f2e2
Sufianmahmood <100502783+Sufianmahmood@users.noreply.github.com>
2024-10-13 05:04:29 +0000

    cnverstion  of data types

---
+++ 01_basics/converstion.js
@@ -0,0 +1,22 @@
+//let score = "sufian"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc => NAN
+// true =>1  , false => 0
+
+let isLoggedIn =  1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+// 1=> true,  0-> false
+// ""=> false
+//"sufian"=> true
+
+let Number = 54
+let stringNumber= String(Number)
+console.log(typeof(stringNumber));
--- datatype.js
+++
@@ -1,4 +0,0 @@
-"use strict";//treat all code as newer version
-
-//alert(3+3) // we are using node not browser
-
